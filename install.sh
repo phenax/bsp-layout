259485ea9d960c0e5df7cdc2c513acf79da4b8b9
Akshay Nair <phenax5@gmail.com>
2020-03-31 15:39:46 +0530

    Adds local mode for installer + Adds simple config file

--- install.sh
+++ install.sh
@@ -8,21 +8,34 @@ CONFIG_DIR="${XDG_CONFIG_HOME:-"$HOME/.config"}/bsp-layout"; # NOTE: For the fut
 rm -rf $INSTALL_DIR;
 rm -rf $BINARY;
 
+# Un install is just re-install minus the install
 if [[ "$1" == "uninstall" ]]; then
   echo "Uninstalled bsp-layout";
   exit 0;
 fi
 
+TMP_DIR=$(mktemp -d /tmp/bsp-layout-install.XXXXX);
+
+# Clone to local directory
+if [[ ! "$1" == "local" ]]; then
+  git clone https://github.com/phenax/bsp-layout.git $TMP_DIR/clone;
+  cd $TMP_DIR/clone;
+fi
+
+# Create config if it doesn't already exist
+mkdir -p "$CONFIG_DIR";
+[[ -f "$CONFIG_DIR/layoutrc" ]] && cp exampleconfig "$CONFIG_DIR/layoutrc";
+
 # Copy contents files to install directory
 echo "Copying files..." &&
 mkdir -p $INSTALL_DIR &&
 cp -r src/* $INSTALL_DIR/ &&
-chmod +x src/layout.sh &&
-chmod +x src/layouts/*.sh &&
 
 # Create binary executable
 echo "Creating binary..." &&
 ln -s $INSTALL_DIR/layout.sh $BINARY &&
 
-echo "Installed bsp-layout";
+# Remove clone directory
+rm -rf $TMP_DIR;
 
+echo "Installed bsp-layout";
