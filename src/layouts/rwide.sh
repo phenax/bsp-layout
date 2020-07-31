a16ef8a46e3c57785c58c78ef38106d878e5daba
Akshay Nair <phenax5@gmail.com>
2020-07-31 17:07:17 +0530

    Adds reverse layouts

---
+++ src/layouts/rwide.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+source "$ROOT/utils/common.sh";
+source "$ROOT/utils/layout.sh";
+source "$ROOT/utils/config.sh";
+
+master_size=$WIDE_RATIO;
+
+node_filter="!hidden";
+
+execute_layout() {
+  while [[ ! "$#" == 0 ]]; do
+    case "$1" in
+      --master-size) master_size="$2"; shift; ;;
+      *) echo "$x" ;;
+    esac;
+    shift;
+  done;
+
+  # ensure the count of the master child is 1, or make it so
+  local nodes=$(bspc query -N '@/2' -n .descendant_of.window.$node_filter);
+  local win_count=$(echo "$nodes" | wc -l);
+
+  if [ $win_count -ne 1 ]; then
+    local new_node=$(bspc query -N '@/2' -n last.descendant_of.window.$node_filter | head -n 1);
+
+    if [ -z "$new_node" ]; then
+      new_node=$(bspc query -N '@/1' -n last.descendant_of.window.$node_filter | head -n 1);
+    fi
+
+    local root=$(echo "$nodes" | head -n 1);
+
+    # move everything into 2 that is not our new_node
+    for wid in $(bspc query -N '@/2' -n .descendant_of.window.$node_filter | grep -v $root); do
+      bspc node "$wid" -n '@/1';
+    done
+
+    bspc node "$root" -n '@/2';
+  fi
+
+  rotate '@/' horizontal 90;
+  rotate '@/2' vertical 90;
+
+  local stack_node=$(bspc query -N '@/1' -n);
+  for parent in $(bspc query -N '@/1' -n .descendant_of.!window.$node_filter | grep -v $stack_node); do
+    rotate $parent vertical 90;
+  done
+
+  auto_balance '@/1';
+
+  local mon_height=$(jget height "$(bspc query -T -m)");
+
+  local want=$(echo "$master_size * $mon_height" | bc | sed 's/\..*//');
+  local have=$(jget height "$(bspc query -T -n '@/2')");
+
+  bspc node '@/2' --resize top 0 $((have - want));
+}
+
+execute_layout "$@";
