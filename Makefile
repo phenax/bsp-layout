7b055cea910a6263d48d84d4f19e51e46f004df6
Akshay Nair <phenax5@gmail.com>
2020-12-04 22:26:18 +0530

    Uses makefile for installation

---
+++ Makefile
@@ -0,0 +1,30 @@
+# bsp-layout
+
+VERSION = 0.0.5
+
+PREFIX = ${DESTDIR}/usr/local
+MANPREFIX = ${PREFIX}/share/man
+SRCPREFIX = ${DESTDIR}/usr/lib
+
+BINARY_PATH = ${PREFIX}/bin
+MAN_PATH = ${MANPREFIX}/man1
+SRC_PATH = ${SRCPREFIX}/bsp-layout
+
+MANPAGE = ${MAN_PATH}/bsp-layout.1
+
+uninstall:
+	rm -f ${BINARY_PATH}/bsp-layout ${MAN_PATH}/bsp-layout.1
+	rm -rf ${SRC_PATH}
+
+install: uninstall
+	mkdir -p ${BINARY_PATH} ${SRC_PATH} ${MAN_PATH}
+	cp -r src/* ${SRC_PATH}/ # Source files
+	sed "s/{{VERSION}}/${VERSION}/g" < src/layout.sh > ${SRC_PATH}/layout.sh # Update version
+	sed "s/{{VERSION}}/${VERSION}/g" < bsp-layout.1 > ${MANPAGE} # Update version
+	chmod +x ${SRC_PATH}/layouts/*.sh
+	chmod +x ${SRC_PATH}/layout.sh
+	chmod 644 ${MANPAGE}
+	ln -s ${SRC_PATH}/layout.sh ${BINARY_PATH}/bsp-layout
+	echo "Unstalled bsp-layout"
+
+.PHONY: install uninstall
--- install.sh
+++ install.sh
@@ -1,60 +1,14 @@
 #!/usr/bin/env bash
 
-VERSION="0.0.5";
-
-INSTALL_DIR=/usr/lib/bsp-layout;
-BINARY=/usr/local/bin/bsp-layout;
-MAN_PAGE=/usr/local/man/man1/bsp-layout.1;
-
-# Clean up
-rm -rf $INSTALL_DIR;
-rm -rf $BINARY;
-rm -rf $MAN_PAGE;
-
-# Un install is just re-install minus the install
-if [[ "$1" == "uninstall" ]]; then
-  echo "Uninstalled bsp-layout";
-  exit 0;
-fi
-
 TMP_DIR=$(mktemp -d /tmp/bsp-layout-install.XXXXX);
 
-# Clone to local directory
+## Clone to local directory
 if [[ ! "$1" == "local" ]]; then
   git clone https://github.com/phenax/bsp-layout.git $TMP_DIR/clone;
   cd $TMP_DIR/clone;
 fi
 
-inject_version() {
-  sed "s/{{VERSION}}/$VERSION/g" < $1 > $2;
-}
+make install;
 
-_() { $* || (echo "Install failed at: $@" && exit 1); }
+rm -rf $TMP_DIR;
 
-# Copy contents files to install directory
-echo "Copying files...";
-_ mkdir -p $INSTALL_DIR;
-_ cp -r src/* $INSTALL_DIR/;
-_ inject_version "src/layout.sh" "$INSTALL_DIR/layout.sh"; # Replace version number
-_ chmod +x $INSTALL_DIR/layouts/*.sh;
-_ chmod +x $INSTALL_DIR/layout.sh;
-
-# Install manpage
-_ inject_version "bsp-layout.1" "$MAN_PAGE";
-_ chmod 644 "$MAN_PAGE";
-
-# Create binary executable
-echo "Creating binary...";
-_ ln -s $INSTALL_DIR/layout.sh $BINARY;
-
-# Remove clone directory
-_ rm -rf $TMP_DIR;
-
-echo "Installed bsp-layout";
-
-# Check for dependencies
-for dep in bc bspc; do
-  !(which $dep >/dev/null 2>&1) && echo "[Missing dependency] bsp-layout needs $dep installed";
-done;
-
-exit 0;
