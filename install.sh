9b9a25c696c6ce11742c693a48b9bd343278303c
Zakaria Barkouk <b.zakaria@zohomail.com>
2022-08-03 12:30:17 +0100

    Improve: Remove useless ';'

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-TMP_DIR=$(mktemp -d /tmp/bsp-layout-install.XXXXX);
+TMP_DIR=$(mktemp -d /tmp/bsp-layout-install.XXXXX)
 
 ## Clone to local directory
 if [[ ! "$1" == "local" ]]; then
-  git clone https://github.com/phenax/bsp-layout.git $TMP_DIR;
-  cd $TMP_DIR;
+  git clone https://github.com/phenax/bsp-layout.git $TMP_DIR
+  cd $TMP_DIR
 fi
 
-VERSION=$(git describe --tags --abbrev=0);
+VERSION=$(git describe --tags --abbrev=0)
 
-sudo make VERSION="$VERSION" install || exit 1;
+sudo make VERSION="$VERSION" install || exit 1
 
 # Check for dependencies
 for dep in bc bspc bash man; do
-  !(which $dep >/dev/null 2>&1) && echo "[Missing dependency] bsp-layout needs $dep installed";
-done;
+  !(which $dep >/dev/null 2>&1) && echo "[Missing dependency] bsp-layout needs $dep installed"
+done
 
-rm -rf "$TMP_DIR";
+rm -rf "$TMP_DIR"
 
-exit 0;
+exit 0
