cbadea81b8f8bdee8fd603a4fc7d7ad68cc34ecf
Zakaria Barkouk <b.zakaria@zohomail.com>
2022-08-19 18:04:49 +0100

    Fix

--- src/utils/common.sh
+++ src/utils/common.sh
@@ -11,7 +11,7 @@ jget() {
 # () -> ()
 check_dependencies () {
   for dep in bc bspc man; do
-    (which $dep >/dev/null 2>&1) || {
+    !(which $dep >/dev/null 2>&1) && {
       echo "[Missing dependency] bsp-layout needs $dep installed"
       exit 1
     }
